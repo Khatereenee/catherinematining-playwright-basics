6cd97b39b984fd94eee2ad0c4d8a5e1d1e31c315
Catherine Matining <catherinematining18@gmail.com>
2025-07-06 10:45:58 +0800

    Add saucedemo login spec file and test - screenshots folder contents

--- tests/saucedemo/login.spec.js
+++ tests/saucedemo/login.spec.js
@@ -3,20 +3,30 @@ import { test, expect } from '@playwright/test';
 
 
 // test function to verify login with a valid credentials
-test.only('Should login successfully', async ({ page }) => {
+test('Should login successfully', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
   await page.locator('[data-test="username"]').fill('visual_user');
   await expect(page.locator('[data-test="username"]')).toHaveValue('visual_user');
   await page.locator('[data-test="password"]').fill('secret_sauce');
   await expect(page.locator('[data-test="password"]')).toHaveValue('secret_sauce');
   await page.locator('[data-test="login-button"]').click();
-
-  await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html')
-
+  await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
   await expect(page.getByText('Swag Labs')).toBeVisible();
-
-  await page.screenshot({path: 'test - screenshots/successful-logon.png', fullPage: true })
+  await page.screenshot({path: 'test - screenshots/successful-login.png', fullPage: true })
 });
+
+// test function to verify login with a invalid credentials
+test('Should not login successfully', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').fill('invalid_user');
+  await expect(page.locator('[data-test="username"]')).toHaveValue('invalid_user');
+  await page.locator('[data-test="password"]').fill('invalid_sauce');
+  await expect(page.locator('[data-test="password"]')).toHaveValue('invalid_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await expect(page).toHaveURL('https://www.saucedemo.com/');
+  await page.screenshot({path: 'test - screenshots/unsuccessful-login.png', fullPage: true })
+});
+
 
 
 
